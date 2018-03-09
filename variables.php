234b9e296ae3f34355457d20944390d6bac450cd
gauravtomar1995 <gaurav.seowhizz@gmail.com>
2018-03-09 13:29:28 +0530

    intro to php

---
+++ variables.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>how to use variable</title>
+</head>
+
+<body>
+
+<?php
+// "$" represents the variable. 
+
+$name = "i will tell you how to use variable in php"; 
+echo $name;
+?>
+
+<?php
+//"."period symbol is used to add more strings or character.
+
+$name1 = "ferrari";
+echo "my fav car is".$name1."this is the best car";
+
+?>
+
+
+
+</body>
+
+
+
+
+</html>
+
