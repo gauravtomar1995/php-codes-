2107308268c3600ef6a6fbf61d06711f96e3840d
gauravtomar1995 <36945798+gauravtomar1995@users.noreply.github.com>
2018-03-09 02:54:33 -0800

    constants

---
+++ constants.php
@@ -0,0 +1,22 @@
+
+<html>
+<head>
+<title>constants</title>
+</head>
+
+<body>
+<?php
+// DEFINE IS A FUNCTION WITH CONSTANT "USERNAME" & CONSTANT SHOULD ALWAYS BE IN CAPITAL LETTERS.
+
+define('USERNAME', 'John luise');
+echo USERNAME;
+//there are different types of constants 
+echo PHP_VERSION;//this constant tells us about version of php
+ECHO PHP_OS;//this constant tells us about windows/os
+?>
+
+
+</body>
+
+
+</html>
