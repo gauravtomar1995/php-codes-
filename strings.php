266446c582095436e69c6d767d1d155409431c5f
gauravtomar1995 <gaurav.seowhizz@gmail.com>
2018-03-09 13:29:20 +0530

    intro to php

---
+++ strings.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title> how to concetenate strings </title>
+</head>
+<body>
+<?php
+$name1 = "john";
+$name2 = "luise";
+$name1.=$name2;
+
+//"$name1 is concetenate with $name2 by using '.'symbol" when we call $name1 it will concetenate $name2 and will display "john luise" in series.
+//but when we call $name2 it will only diplay "luise".
+
+echo $name1;
+
+
+?>
+
+
+</body>
+
+
+</html>
