e53468b641e5c2c271c75a1c048824907887cd09
gauravtomar1995 <gaurav.seowhizz@gmail.com>
2018-03-09 13:28:55 +0530

    intro to php

---
+++ hello_world.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>first php code</title>
+</head>
+<body>
+
+<!-- if you want to run it on web server you have to type "localhost/filename" -->
+<h1>this is my first code using html in php</h1>
+
+     code output:
+<?php   
+
+//if you have to escape characters you have to use slash before quotes
+echo "hello world is a \"keyword\"";
+
+echo 'I\'m a professional web developer';
+
+?>
+</body>
+</html>
