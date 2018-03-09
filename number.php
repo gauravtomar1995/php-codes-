5b4d75f1755ba40510fdd532affd56eb8b2e9acb
gauravtomar1995 <gaurav.seowhizz@gmail.com>
2018-03-09 13:29:11 +0530

    intro to php

---
+++ number.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>types of functions in numbers</title>
+</head>
+<body>
+<?php
+// simple addition
+$add=3+3;
+
+echo $add;
+?>
+<?php
+//how to round figure the decimals to the nearest numbers
+
+
+$num1 = 3.456;
+$result = round($num1);
+
+echo $result;
+
+
+?>
+
+<?php
+
+//formatting the numbers
+
+$numb = 123456572332897234932049;
+$result = number_format($numb);
+
+echo $result;
+
+
+?>
+
+</body>
+</html>
