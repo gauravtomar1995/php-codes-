6b36f4d6ae637f232410e8a829ec59435dc32831
gauravtomar1995 <36945798+gauravtomar1995@users.noreply.github.com>
2018-03-12 04:03:45 -0700

    form
    how to create form using php and html
    use both files "userfrom.html" and "garb.php" to get output

---
+++ garb.php
@@ -0,0 +1,12 @@
+<?php
+
+$first_name = $_POST['name'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$age = $_POST['Age'];
+$comment = $_POST['comments'];
+echo "name is" .$first_name. "<br />" ."email address is"  .$email. "<br />".
+"your gender is ".$gender."<br />"."your age is ".$age."<br />".$comment;
+
+
+?>
---
+++ userform.php
@@ -0,0 +1,47 @@
+<html>
+
+<head>
+</title>userform</title>
+</head>
+
+<body>
+    
+	<form action = "" method = "">
+	
+	    <p>Name:   <input type = "text" name ="name" size = "20" maxlength = "40" /></p>
+		<p>Email:  <input type = "text" name ="email" size = "20" maxlength = "40" /></p>
+		<p>Gender: <input type = "radio" name ="gender" value = "M" /> Male
+		           <input type = "radio" name ="gender" value = "f" /> female
+		</p>
+		
+		<p> 
+		  <select name = "Age">
+                 <option value = "0-30">Below 30</option>		
+		         <option value = "30-60">between 30-60</option>
+				 <option value = "60-150">between 60-150</option>
+		  </select>
+        </p>		
+		<p>comments:<textarea name="comments" rows = "3" cols ="40"></textarea></p>
+		
+		<p><input type = "submit" name ="submit" value= "submit"/></p>
+		
+	</form>
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+</body>
+
+</html>
